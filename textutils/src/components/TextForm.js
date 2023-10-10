eeb5e4e3503d745b4347fbc49138af2482789b7e
mridula <mridula@gmail.com>
2023-10-10 11:59:00 +0530

    addedd text form

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -1,7 +1,7 @@
 import React,{useState} from 'react';
 export default function TextForm(props) {
     const handleUpClick = ()=>{
-        console.log("Uppercase was clicked"); 
+        console.log("Uppercase was clicked");
         let newText = text.toUpperCase();
         setText(newText)
     }
@@ -15,8 +15,8 @@ export default function TextForm(props) {
         <div>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <textarea className="form-control" value = {text} onChange = {handleOnChange}id="exampleFormControlTextarea1" rows="8"></textarea>
-            </div>
+            <textarea className="form-control" value = {text} onChange = {handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
+        </div>
             <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
         </div>
         <div className="container my-3">
